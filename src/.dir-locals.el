0a65854e11b9bc34bc109d772893a3205f0d1209
Sébastien Brisard <sbrisard@users.noreply.github.com>
2021-09-27 09:18:56 +0200

    [Emacs] Create directory local variables

---
+++ src/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((latex-mode . ((TeX-master . "LS-intro.tex")
+		(TeX-engine . xetex)
+		(fill-column . 80)
+		(coding . utf-8))))
